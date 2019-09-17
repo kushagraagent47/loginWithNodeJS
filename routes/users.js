35f52d9c14bb71fbf79fc85d99ffc1286d3ec1ac
kushagraagent47 <kushagraagent47@gmail.com>
2019-09-18 03:01:23 +0530

    first commit

---
+++ routes/users.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+
+//LOGIN
+router.get('/login', (req,res) => res.render('login'));
+
+//Register
+router.get('/register', (req,res) => res.render('register'));
+
+
+module.exports = router;
