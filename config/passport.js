73aed8b2112658779f69c75198f1bcb00b4ebd90
kushagraagent47 <kushagraagent47@gmail.com>
2019-10-18 22:51:25 +0530

    Configuring OAUTH

--- config/passport.js
+++ config/passport.js
@@ -12,7 +12,7 @@ module.exports = function(passport) {
         new GoogleStrategy({
             clientID: keys.googleClientID,
             clientSecret: keys.googleclientSecret,
-            callback:'/auth/google/callback',
+            callbackURL:'/auth/google/callback',
             proxy: true
         },(accessToken, refreshToken, profile, done) => {
             console.log(accessToken);
