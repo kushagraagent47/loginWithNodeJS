bb8bafa0adb892bae1bd85d8aadb4c893fd1f33d
kushagraagent47 <kushagraagent47@gmail.com>
2019-10-18 23:32:19 +0530

    Configuring OAUTH

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
+require('./config/passport')(passport);
 
 router.get('/google', passport.authenticate('google', {scope: ['profile', 'email']}));
 
