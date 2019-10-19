721de48b6e7476a2fe2ffe3cff7849f5f942a1d1
kushagraagent47 <kushagraagent47@gmail.com>
2019-10-19 10:47:06 +0530

    Configured OAUTH But login not working

--- routes/auth.js
+++ routes/auth.js
@@ -8,4 +8,10 @@ require('../config/passport')(passport);
 router.get('/google', passport.authenticate('google', 
 {scope: ['profile', 'email']}));
 
+router.get('/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/login' }),(req, res) => {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
+
 module.exports = router;
